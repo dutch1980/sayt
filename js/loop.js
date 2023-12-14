e570e60dd6bd6204084297358afd752a737c4ab5
dutch1980 <dutch.shuls@gmail.com>
2023-12-14 20:48:44 +0500

    мой первый сайт

---
+++ js/loop.js
@@ -0,0 +1,12 @@
+const mayArr=[];
+for(let i=0; i<10; i++){
+    mayArr.push(i+1);
+    
+}
+console.log(mayArr);
+for (let j=0; j<mayArr.length;j++){
+    console.log(mayArr[j]);
+}
+for(let val of mayArr){
+    console.log(val);
+}
